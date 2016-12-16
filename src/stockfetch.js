e9a75e86291afacbc911aee108eb9703170201eb
achernyak <artemchernyak@gmail.com>
2016-12-16 08:19:14 -0600

    testing

---
+++ src/stockfetch.js
@@ -0,0 +1,67 @@
+var fs = require('fs');
+var http = require('http');
+
+var Stockfetch = function() {
+  this.readTickersFile = function(filename, onError) {
+    var self = this;
+
+    var processResponse = function(err, data) {
+      if(err)
+        onError('Error reading file: ' + filename);
+      else {
+        var tickers = self.parseTickers(data.toString());
+        if(tickers.length === 0)
+          onError('File ' + filename + ' has invalid content');
+        else
+          self.processTickers(tickers);
+      }
+    };
+
+    fs.readFile(filename, processResponse);
+  };
+
+  this.parseTickers = function(content) {
+    var isInrightFormat = function(str) {
+      return str.trim().length !== 0 && str.indexOf(' ') < 0;
+    };
+    return content.split('\n').filter(isInrightFormat);
+  };
+
+  this.processTickers = function(tickers) {
+    var self = this;
+    self.tickersCount = tickers.length;
+    tickers.forEach(function(ticker) { self.getPrice(ticker) ;});
+  };
+
+  this.tickersCount = 0;
+
+  this.http = http;
+
+  this.getPrice = function(symbol) {
+    var url = 'http://ichart.finance.yahoo.com/table.csv?s=' + symbol;
+    this.http.get(url, this.processResponse.bind(this, symbol))
+      .on('error', this.processHttpError.bind(this, symbol));
+  };
+
+  this.processResponse = function(symbol, response) {
+    var self = this;
+
+    if(response.statusCode === 200) {
+      var data = '';
+      response.on('data', function(chunk) { data += chunk; });
+      response.on('end', function() { self.parsePrice(symbol, data); });
+    } else {
+      self.processError(symbol, response.statusCode);
+    }
+  };
+
+  this.parsePrice = function() {};
+
+  this.processError = function() {};
+
+  this.processHttpError = function(ticker, error) {
+    this.processError(ticker, error.code);
+  };
+};
+
+module.exports = Stockfetch;
---
+++ test/stockfetch-test.js
@@ -0,0 +1,213 @@
+var expect = require('chai').expect;
+var sinon = require('sinon');
+var fs = require('fs');
+var Stockfetch = require('../src/stockfetch');
+
+describe('Stockfetch tests', function() {
+  var stockfetch;
+  var sandbox;
+
+  beforeEach(function() {
+    stockfetch = new Stockfetch();
+    sandbox = sinon.sandbox.create();
+  });
+
+  afterEach(function() {
+    sandbox.restore();
+  });
+
+  it('should pass this canary test', function() {
+    expect(true).to.be.true;
+  });
+
+  it('read should invoke error handler for invalid file', function(done) {
+    var onError = function(err) {
+      expect(err).to.be.eql('Error reading file: InvalidFile');
+      done();
+    };
+
+    sandbox.stub(fs, 'readFile', function(fileName, callback) {
+      callback(new Error('failed'));
+    });
+
+    stockfetch.readTickersFile('InvalidFile', onError);
+  });
+
+  it('read should invoke processTickets for valid file', function(done) {
+    var rawData = 'GOOG\nAAPL\nORCL\nMSFT';
+    var parsedData = ['GOOG', 'AAPL', 'ORCL', 'MSFT'];
+
+    sandbox.stub(stockfetch, 'parseTickers')
+      .withArgs(rawData).returns(parsedData);
+
+    sandbox.stub(stockfetch, 'processTickers', function(data) {
+      expect(data).to.be.eql(parsedData);
+      done();
+    });
+
+    sandbox.stub(fs, 'readFile', function(fileName, callback) {
+      callback(null, rawData);
+    });
+
+    stockfetch.readTickersFile('tickers.txt');
+  });
+
+  it('read should return error if given file is empty', function(done) {
+    var onError = function(err) {
+      expect(err).to.be.eql('File tickers.txt has invalid content');
+      done();
+    };
+
+    sandbox.stub(stockfetch, 'parseTickers').withArgs('').returns([]);
+
+    sandbox.stub(fs, 'readFile', function(fileName, callback) {
+      callback(null, '');
+    });
+
+    stockfetch.readTickersFile('tickers.txt', onError);
+  });
+
+  it('parseTickers should return tickers', function() {
+    expect(stockfetch.parseTickers("A\nB\nC")).to.be.eql(['A', 'B', 'C']);
+  });
+
+  it('parseTickers should return empty array for empty content', function() {
+    expect(stockfetch.parseTickers("")).to.be.eql([]);
+  });
+
+  it('parseTickers should return empty array for white-space', function() {
+    expect(stockfetch.parseTickers("  ")).to.be.eql([]);
+  });
+
+  it('parseTickers should ignore unexpected format in content', function() {
+    var rawData = "AAPL  \nBla h\nGOOG\n\n    ";
+    expect(stockfetch.parseTickers(rawData)).to.be.eql(['GOOG']);
+  });
+
+  it('processTickers should call getPrice for each ticker symbol', function() {
+    var stockfetchMock = sandbox.mock(stockfetch);
+    stockfetchMock.expects('getPrice').withArgs('A');
+    stockfetchMock.expects('getPrice').withArgs('B');
+    stockfetchMock.expects('getPrice').withArgs('C');
+
+    stockfetch.processTickers(['A', 'B', 'C']);
+    stockfetchMock.verify();
+  });
+
+  it('processTickers should save tickers count', function() {
+    sandbox.stub(stockfetch, 'getPrice');
+
+    stockfetch.processTickers(['A', 'B', 'C']);
+    expect(stockfetch.tickersCount).to.be.eql(3);
+  });
+
+  it('getPrice should call get on http with valid URL', function(done) {
+    var httpStub = sandbox.stub(stockfetch.http, 'get', function(url) {
+      expect(url)
+        .to.be.eql('http://ichart.finance.yahoo.com/table.csv?s=GOOG');
+      done();
+      return { on: function() {} };
+    });
+
+    stockfetch.getPrice('GOOG');
+  });
+
+  it('getPrice should send a response handler to get', function(done) {
+    var aHandler = function() {};
+
+    sandbox.stub(stockfetch.processResponse, 'bind')
+      .withArgs(stockfetch, 'GOOG')
+      .returns(aHandler);
+
+    var httpStub = sandbox.stub(stockfetch.http, 'get',
+    function(url, handler) {
+      expect(handler).to.be.eql(aHandler);
+      done();
+      return { on: function() {} };
+    });
+
+    stockfetch.getPrice('GOOG');
+  });
+
+  it('getPrice should register handler for failure to reach host',
+  function(done) {
+    var errorHandler = function() {};
+
+    sandbox.stub(stockfetch.processHttpError, 'bind')
+      .withArgs(stockfetch, 'GOOG')
+      .returns(errorHandler);
+
+    var onStub = function(event, handler) {
+      expect(event).to.be.eql('error');
+      expect(handler).to.be.eql(errorHandler);
+      done();
+    };
+
+    sandbox.stub(stockfetch.http, 'get').returns({ on: onStub });
+
+    stockfetch.getPrice('GOOG');
+  });
+
+  it('processResponse should call parsePrice with valid data', function() {
+    var dataFunction;
+    var endFunction;
+
+    var response = {
+      statusCode: 200,
+      on: function(event, handler) {
+        if(event === 'data') dataFunction = handler;
+        if(event === 'end') endFunction = handler;
+      }
+    };
+
+    var parsePriceMock = sandbox.mock(stockfetch)
+      .expects('parsePrice').withArgs('GOOG', 'some data');
+
+    stockfetch.processResponse('GOOG', response);
+    dataFunction('some ');
+    dataFunction('data');
+    endFunction();
+
+    parsePriceMock.verify();
+  });
+
+  it('processResponse should call processError if response failed',
+    function() {
+      var response = { statusCode: 404 };
+
+      var processErrorMock = sandbox.mock(stockfetch)
+        .expects('processError')
+        .withArgs('GOOG', 404);
+
+      stockfetch.processResponse('GOOG', response);
+      processErrorMock.verify();
+  });
+
+  it('processResponse should call processError only if response failed',
+    function() {
+      var response = {
+        statusCode: 200,
+        on: function() {}
+      };
+
+      var processErrorMock = sandbox.mock(stockfetch)
+        .expects('processError').never();
+
+      stockfetch.processResponse('GOOG', response);
+      processErrorMock.verify();
+  });
+
+  it('processHttpError should call processHttpError with error details',
+    function() {
+      var processErrorMock = sandbox.mock(stockfetch)
+        .expects('processError').withArgs('GOOG', '...error code...');
+
+      var error = { code: '...error code...'};
+      stockfetch.processHttpError('GOOG', error);
+      processErrorMock.verify();
+    });
+
+  var data = "Date,Open,High,Low,Close,Volume,Adj Close\n\
+  2016-12-15,797.340027,803.00,792.919983,797.849976,1623700,797.849976\n\
+  2016-12-14,797.400024,804.00,794.01001,797.070007,1664500,797.070007";
+});
